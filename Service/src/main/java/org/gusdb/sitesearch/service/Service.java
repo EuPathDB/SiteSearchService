45611856d746adc7d52e98eaf7bc3f9d84dd18dd
Ryan Doherty <rdoherty@upenn.edu>
2020-01-22 16:46:53 -0500

    Move build-status endpoint logic to a utility for use in WDK

--- Service/src/main/java/org/gusdb/sitesearch/service/Service.java
+++ Service/src/main/java/org/gusdb/sitesearch/service/Service.java
@@ -2,17 +2,7 @@ package org.gusdb.sitesearch.service;
 
 import static org.gusdb.fgputil.FormatUtil.urlEncodeUtf8;
 
-import java.io.BufferedReader;
-import java.io.FileNotFoundException;
-import java.io.FileReader;
-import java.io.IOException;
-import java.nio.file.Paths;
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.HashMap;
-import java.util.LinkedHashMap;
 import java.util.List;
-import java.util.Map;
 import java.util.Optional;
 import java.util.function.Function;
 import java.util.stream.Collectors;
@@ -27,8 +17,7 @@ import javax.ws.rs.core.Response;
 
 import org.apache.log4j.Logger;
 import org.gusdb.fgputil.FormatUtil;
-import org.gusdb.fgputil.FormatUtil.Style;
-import org.gusdb.fgputil.runtime.GusHome;
+import org.gusdb.fgputil.runtime.BuildStatus;
 import org.gusdb.sitesearch.service.metadata.CategoriesMetadata;
 import org.gusdb.sitesearch.service.metadata.DocumentField;
 import org.gusdb.sitesearch.service.metadata.DocumentType;
@@ -197,39 +186,6 @@ public class Service {
   @Path("/build-status")
   @Produces(MediaType.TEXT_PLAIN)
   public Response getBuildStatus() {
-    String statusFile = Paths.get(GusHome.getGusHome(), ".buildlog", "git_status").toAbsolutePath().toString();
-    List<Map<String,String>> records = new ArrayList<>();
-    try (BufferedReader br = new BufferedReader(new FileReader(statusFile))) {
-      Map<String,String> buildRecord = new LinkedHashMap<>();
-      while (br.ready()) {
-        String line = br.readLine().trim();
-        if (line.isEmpty()) {
-          if (!buildRecord.isEmpty()) {
-            records.add(buildRecord);
-            buildRecord = new LinkedHashMap<>();
-          }
-        }
-        else {
-          String[] tokens = line.split("\\s+", 2);
-          buildRecord.put(tokens[0], tokens.length > 1 ? tokens[1] : "");
-        }
-      }
-      if (!buildRecord.isEmpty()) {
-        records.add(buildRecord);
-      }
-    }
-    catch (IOException e) {
-      throw new SiteSearchRuntimeException("Could not query build status", e);
-    }
-    Map<String,Map<String,String>> lastBuildRecords = new HashMap<>();
-    for (Map<String,String> record : records) {
-      lastBuildRecords.put(record.get("Project:")+"."+record.get("Component:"), record);
-    }
-    List<String> sortedKeys = new ArrayList<>(lastBuildRecords.keySet());
-    Collections.sort(sortedKeys);
-    String result = sortedKeys.stream()
-      .map(key -> FormatUtil.prettyPrint(lastBuildRecords.get(key), Style.MULTI_LINE))
-      .collect(Collectors.joining("\n"));
-    return Response.ok(result).build();
+    return Response.ok(BuildStatus.getLatestBuildStatus()).build();
   }
 }
