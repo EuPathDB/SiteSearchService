042ec75cd423e41e5fd98e892443f20087d0884e
Elizabeth Paige Harper <foxcapades.io@gmail.com>
2023-06-21 11:06:28 -0400

    updates

--- src/main/java/org/gusdb/sitesearch/service/Service.java
+++ src/main/java/org/gusdb/sitesearch/service/Service.java
@@ -2,16 +2,12 @@ package org.gusdb.sitesearch.service;
 
 import java.io.IOException;
 import java.io.OutputStream;
+import java.net.URL;
+import java.net.URLEncoder;
+import java.nio.charset.Charset;
 import java.util.Optional;
 
-import javax.ws.rs.Consumes;
-import javax.ws.rs.DefaultValue;
-import javax.ws.rs.GET;
-import javax.ws.rs.POST;
-import javax.ws.rs.Path;
-import javax.ws.rs.Produces;
-import javax.ws.rs.QueryParam;
-import javax.ws.rs.WebApplicationException;
+import javax.ws.rs.*;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
 import javax.ws.rs.core.StreamingOutput;
@@ -26,6 +22,8 @@ import org.gusdb.fgputil.web.MimeTypes;
 import org.gusdb.sitesearch.service.metadata.Metadata;
 import org.gusdb.sitesearch.service.request.SearchRequest;
 import org.gusdb.sitesearch.service.server.Server.Context;
+import org.json.JSONArray;
+import org.json.JSONException;
 import org.json.JSONObject;
 
 @Path("/")
@@ -61,6 +59,77 @@ public class Service {
   @Produces(MimeTypes.ND_JSON)
   public Response getStreamingResults(String body) {
     return handleStreamRequest(getSolr(), new SearchRequest(new JSONObject(body), false, true, false));
+  }
+
+  /**
+   * SOLR Response:
+   *
+   * <pre>
+   * {
+   *   "suggest": {
+   *     "default": {
+   *       "<term>": {
+   *         "numFound": 10,
+   *         "suggestions": [
+   *           {
+   *             "term": "<match>",
+   *             "weight": 0,
+   *             "payload": ""
+   *           }
+   *         ]
+   *       }
+   *     }
+   *   }
+   * }
+   * </pre>
+   */
+  @GET
+  @Path("/suggest")
+  @Produces(MediaType.APPLICATION_JSON)
+  public Response getSuggestions(@QueryParam("searchText") String searchText) {
+    // Return an empty list for requests that contain no search text or a search
+    // term that is fewer than 3 characters.
+    if (searchText == null || searchText.isBlank() || searchText.trim().length() < 3)
+      return Response.ok("[]", MediaType.APPLICATION_JSON_TYPE).build();
+
+    // Build the SOLR request URL
+    var url = (String) RESTServer.getApplicationContext().get(Context.SOLR_URL);
+    var ep = url.endsWith("/") ? url + "suggest" : url + "/suggest";
+    var q = ep + "?suggest.q=" + URLEncoder.encode(searchText, Charset.defaultCharset());
+
+    // Open the connection to SOLR
+    try(var stream = new URL(q).openConnection().getInputStream()) {
+      // Parse the response
+      var node = new JSONObject(new String(stream.readAllBytes()));
+
+      node = node.getJSONObject("suggest")
+        .getJSONObject("default");
+
+      if (node.length() > 1)
+        throw new InternalServerErrorException("unexpected response from SOLR: suggest.default object contained more than one suggestion set");
+      if (node.length() < 1)
+        throw new InternalServerErrorException("unexpected response from SOLR: suggest.default object contained no suggestion sets");
+
+      node = node.getJSONObject(node.keys().next());
+
+      if (node.getInt("numFound") < 1)
+        return Response.ok("[]", MediaType.APPLICATION_JSON_TYPE).build();
+
+      var suggestions = node.getJSONArray("suggestions");
+      var out = new JSONArray(suggestions.length());
+
+      for (var i = 0; i < suggestions.length(); i++) {
+        out.put(suggestions.getJSONObject(i).getString("term"));
+      }
+
+      return Response.ok(out.toString(), MediaType.APPLICATION_JSON_TYPE).build();
+    } catch (IOException e) {
+      LOG.error("failed to connect to SOLR: ", e);
+      throw new InternalServerErrorException("failed to connect to SOLR");
+    } catch (JSONException e) {
+      LOG.error("could not parse JSON response from SOLR", e);
+      throw new InternalServerErrorException("could not parse JSON response from SOLR: " + e.getMessage());
+    }
   }
 
   @GET
